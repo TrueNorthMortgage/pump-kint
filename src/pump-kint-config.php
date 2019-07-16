af14864166cbf7d4537c079de8ab0f14cca78ed8
Sean Rabey <sean@hellopump.com>
2019-07-16 11:58:34 -0600

    Add sd() helper function to display only colored text and die

--- src/pump-kint-config.php
+++ src/pump-kint-config.php
@@ -1,11 +1,18 @@
 <?php
 
 Kint::$aliases[] = 'dd';
+Kint::$aliases[] = 'sd';
 Kint::$enabled_mode = (bool)getenv('DEV_MODE');
 Kint\Renderer\RichRenderer::$folder = false;
 
 function dd(...$vars)
 {
     Kint::dump(...$vars);
+    exit;
+}
+
+function sd(...$vars)
+{
+    s(...$vars);
     exit;
 }
